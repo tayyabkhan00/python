bdd822dd88e378880f32f11fe96588838d87502a
tayyabkhan00 <iron29700@gmail.com>
2025-12-05 15:33:54 +0530

    broadcasting

---
+++ numpy_tutorial/broadcasting.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr=np.array([2,7,11,15])
+print(arr + 3)
+
+a = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+b = np.array([10, 20, 30])
+
+print(a + b)
+# Broadcasting with different shapes
+
