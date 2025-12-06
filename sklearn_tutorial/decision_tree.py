bbb326745e0af09fee89e1e8b231faea228dfe4c
tayyabkhan00 <iron29700@gmail.com>
2025-12-06 15:44:17 +0530

    Decision Tree

---
+++ sklearn_tutorial/decision_tree.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+tree = DecisionTreeClassifier()
+tree.fit(X_train, y_train)
+
+plt.figure(figsize=(12,8))
+plot_tree(tree, filled=True, feature_names=iris.feature_names, class_names=iris.target_names)
+plt.show()
+
+print("Feature Importances:", tree.feature_importances_)
