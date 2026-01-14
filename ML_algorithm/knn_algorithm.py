35a417d1fab40c6fda935bb0d9ca6be8d74f5456
tayyabkhan00 <iron29700@gmail.com>
2026-01-14 15:00:19 +0530

    knn K-Nearest Neighbors (KNN)

---
+++ ML_algorithm/knn_algorithm.py
@@ -0,0 +1,25 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
+# Sample data
+X = [[25], [30], [35], [40], [45]]
+y = [0, 0, 1, 1, 1]
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Feature scaling
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Model
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
