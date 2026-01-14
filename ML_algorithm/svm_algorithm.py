ebafecae4f9ca7de31971e36fb838b61eece6efd
tayyabkhan00 <iron29700@gmail.com>
2026-01-14 17:53:57 +0530

    svm algorithm

--- ML_algorithm/svm_algorithm.py
+++ ML_algorithm/svm_algorithm.py
@@ -1,37 +1,25 @@
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 
 # Sample data
-X = [[25], [30], [35], [40], [45]]
-y = [0, 0, 1, 1, 1]
+X = [[1], [2], [3], [6], [7], [8]]
+y = [0, 0, 0, 1, 1, 1]
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Feature scaling
+# Scaling (very important)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Model
-model = KNeighborsClassifier(n_neighbors=3)
+model = SVC(kernel='rbf', C=1, gamma='scale')
 model.fit(X_train, y_train)
 
 # Prediction
 y_pred = model.predict(X_test)
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
-
-# Example usage
-
-from sklearn.neighbors import KNeighborsClassifier
-
-X = [[40], [45], [50], [70], [75], [80]]  # marks
-y = ['A', 'A', 'A', 'B', 'B', 'B']
-
-model = KNeighborsClassifier(n_neighbors=3)
-model.fit(X, y)
-
-print(model.predict([[55]]))
