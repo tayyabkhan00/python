581ab67d4eefcb8f744f6bdf8fc388201acfa493
tayyabkhan00 <iron29700@gmail.com>
2026-01-21 22:36:42 +0530

    open ai and gemini pro chat completion

--- LLM_GenAI_RAG/chat_competions.py
+++ LLM_GenAI_RAG/chat_competions.py
@@ -1,3 +1,4 @@
+# gemini pro chat completion example
 import google.generativeai as genai
 import os
 
@@ -8,3 +9,17 @@ response = model.generate_content("Give 5 insights from sales data.")
 print(response.text)
 
 
+# open ai chat completion example
+from openai import OpenAI
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4.1-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful teacher."},
+        {"role": "user", "content": "Explain what EMI is in simple words"}
+    ]
+)
+
+print(response.choices[0].message.content)
