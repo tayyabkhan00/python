b761f76ca65d744e051ef821f5fc939cee563223
tayyabkhan00 <iron29700@gmail.com>
2026-01-21 10:08:57 +0530

    import libraries

---
+++ customer_segmentation.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
