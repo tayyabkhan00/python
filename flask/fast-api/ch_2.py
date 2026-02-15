8b09668cc55868cb7ff7b15e872171d8d3898ca8
tayyabkhan00 <iron29700@gmail.com>
2026-02-15 12:42:52 +0530

    learning the basics of flask

---
+++ flask/fast-api/ch_2.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"msg": "Hello Data Scientist"}
+
---
+++ flask/fast-api/student_api/train.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+# Features: [hours_studied, attendance, previous_score]
+X = np.array([
+    [2, 60, 40],
+    [5, 80, 70],
+    [1, 50, 30],
+    [6, 90, 85],
+    [3, 70, 50],
+    [4, 75, 65]
+])
+
+# Target: 0 = Fail, 1 = Pass
+y = np.array([0, 1, 0, 1, 0, 1])
+
+model = LogisticRegression()
+model.fit(X, y)
+
+joblib.dump(model, "/Users/tayyabkhan/python/flask/fast-api/student_api/model.pkl")
+
+print("Model saved successfully!")
