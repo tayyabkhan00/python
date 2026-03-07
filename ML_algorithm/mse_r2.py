edeade85abc12639e1bf2c2daa4fcb768fc4cf22
tayyabkhan00 <iron29700@gmail.com>
2026-03-07 09:55:49 +0530

    recheck

--- ML_algorithm/mse_r2.py
+++ ML_algorithm/mse_r2.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import mean_squared_error, r2_score
 
 y_true = [60, 70, 80]
-y_pred = [58, 72, 79]
+y_pred = [57, 69, 78]
 
 print("MSE:", mean_squared_error(y_true, y_pred))
 print("R2 Score:", r2_score(y_true, y_pred))
@@ -12,3 +12,4 @@ print("R2 Score:", r2_score(y_true, y_pred))
 # Lower = better
 # R² = How good is the line?
 # Higher = better
+# Yes, R² can be negative. It happens when the model performs worse than simply predicting the mean of the target variable.
